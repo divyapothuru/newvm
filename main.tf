5a8f97546c2b950e62b015d1b0026004aaa820c8
divyapothuru <36921186+divyapothuru@users.noreply.github.com>
2021-05-13 15:42:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
 
-  name     = "Azurecli33"
+  name     = "JenkinsAzure"
   location = "North Europe"
   tags = {
         Environment = "Terraform Getting Started"
